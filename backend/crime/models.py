de862fae87e7cfc6732a61fe7807b9549af49839
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-10-21 15:25:36 +0100

    Creating the CrimeLocation model.

--- backend/crime/models.py
+++ backend/crime/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CrimeLocation(models.Model):
+    """Contains the longitude and latitude of a crime."""
+    longitude = models.FloatField()
+    latitude = models.FloatField()
