0701e8a81de6bb9ec19ad4990c00ea000e848f82
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-10-21 16:30:41 +0100

    Creating api endpoint that returns all the crime data locations.

---
+++ backend/crime/serializers.py
@@ -0,0 +1,9 @@
+# crime/serializers.py
+
+from rest_framework import serializers
+from .models import CrimeLocation
+
+class CrimeLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CrimeLocation
+        fields = ('id', 'longitude', 'latitude')
--- backend/crime/views.py
+++ backend/crime/views.py
@@ -1,8 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from .serializers import CrimeLocationSerializer
+from .models import CrimeLocation
+
 
 @api_view(['GET'])
 def test(request):
     data = {"response": "This is a test"}
     return Response(data, status=status.HTTP_200_OK)
+
+class CrimeLocationListView(generics.ListAPIView):
+    queryset = CrimeLocation.objects.all()
+    serializer_class = CrimeLocationSerializer
+
+
