a5db0d4821a4d0b0a63ad2a732e26c4d13e0a148
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-10-21 16:22:57 +0100

    Creating a manage.py custom command that imports the csv data into the models.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,11 @@ INSTALLED_APPS = [
     'corsheaders',
     'crime',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+
+STATIC_URL = '/static/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -128,3 +134,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
