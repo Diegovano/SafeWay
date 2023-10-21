fef8dbe3033ef5b2901af107f52e3d2f8c428c58
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-10-21 21:41:04 +0100

    Dockerising the application.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 // import './App.css'
 import { LatLng } from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { getCrimeData, MarkerData } from "./api";
 
 function App() {
@@ -10,13 +10,15 @@ function App() {
 
   const [crimeLocations, setCrimeLocations] = useState<MarkerData[]>([]);
 
-  useEffect(() => {
-    getCrimeData().then(data => {
-      setCrimeLocations(data);
-    }, err => {
-      console.log(err.message);
-    })
-  }, []);
+  // useEffect(() => {
+  //   getCrimeData().then(data => {
+  //     setCrimeLocations(data);
+  //   }, err => {
+  //     console.log(err.message);
+  //   })
+  // }, []);
+
+  console.log("Hello World");
 
   return (
     <>
@@ -32,28 +34,26 @@ function App() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {crimeLocations.map((item: MarkerData, _index) =>
+          {crimeLocations.map((item: MarkerData, _index) => (
             <Marker position={new LatLng(item.latitude, item.longitude)}>
               <Popup>
-                LAT: {Math.abs(item.latitude)}{item.latitude >= 0 ? 'N' : 'S'} <br></br> LON: {Math.abs(item.longitude)}{item.longitude >= 0 ? 'E' : 'W'}
+                LAT: {Math.abs(item.latitude)}
+                {item.latitude >= 0 ? "N" : "S"} <br></br> LON:{" "}
+                {Math.abs(item.longitude)}
+                {item.longitude >= 0 ? "E" : "W"}
               </Popup>
             </Marker>
-          )}
+          ))}
           <Marker position={new LatLng(55, 0)}>
-            <Popup>
-              item.id
-            </Popup>
+            <Popup>item.id</Popup>
           </Marker>
         </MapContainer>
 
-
-        <h1>
-          Map
-        </h1>
+        <h1>Map</h1>
         <div id="map"></div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
